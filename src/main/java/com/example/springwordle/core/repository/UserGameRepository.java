3c4d47181d0bde40e02af6573154fe75ab0960fb
Bakir Mujkanovic <bakir.mujkanovic01@gmail.com>
2023-10-23 22:11:55 +0200

    feature: add usergame service

--- src/main/java/com/example/springwordle/core/repository/UserGameRepository.java
+++ src/main/java/com/example/springwordle/core/repository/UserGameRepository.java
@@ -1,8 +1,8 @@
 package com.example.springwordle.core.repository;
 
-import com.example.springwordle.core.model.Game;
+import com.example.springwordle.core.model.UserGame;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserGameRepository extends MongoRepository<Game, String> { }
+public interface UserGameRepository extends MongoRepository<UserGame, String> { }
